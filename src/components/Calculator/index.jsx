97385c4a05e855e771c22178e3e8fb22fb65e609
helloleesul <suerish.e@gmail.com>
2024-08-08 18:07:21 +0900

    ✨ feat: 계산기 UI 및 기능

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,90 +1,62 @@
-import { useEffect, useState } from "react";
 import styles from "./Calculator.module.scss";
-import { add } from "calculator-utils";
+import useCalculator from "../../hooks/useCalculator";
 
-export const Calculator = () => {
-  const [input, setInput] = useState(0);
-  const [result, setResult] = useState(0);
-  const [currentSymbol, setCurrentSymbol] = useState("");
-
-  // useEffect(() => {
-  //   // if (!currentSymbol) return;
-  //   console.log("symbol change!");
-  //   setResult(input);
-  // }, [currentSymbol, input]);
-
-  useEffect(() => {
-    if (!input) return;
-    console.log("input change!");
-  }, [input]);
-
-  const handleInput = (e) => {
-    const currentInput = Number(e.target.innerText);
-
-    // 계산 심볼이라면
-    if (isNaN(currentInput)) {
-      // 현재 심볼 상태
-      setCurrentSymbol(e.target.innerText);
-      // setResult(input);
-      onCalculator();
-    }
-    // 숫자라면
-    else {
-      // if (!currentSymbol) {
-      setInput((prev) => {
-        if (prev === 0) {
-          return `${e.target.innerText}`;
-        } else return `${prev}${e.target.innerText}`;
-      });
-      // } else setInput(e.target.innerText);
-    }
-  };
-
-  const onAllClean = () => {
-    setInput(0);
-    setResult(0);
-    setCurrentSymbol("");
-  };
-
-  const onCalculator = () => {
-    switch (currentSymbol) {
-      case "+":
-        console.log(add(result, input));
-        // setResult();
-        break;
-      // case "-":
-      //   console.log(value);
-      //   break;
-      // case "×":
-      //   console.log(value);
-      //   break;
-      // case "÷":
-      //   console.log(value);
-      //   break;
-    }
-  };
+const Calculator = () => {
+  const { input, handleInput, onCalculator, onAllClean } = useCalculator();
 
   return (
     <div className={styles.calculator}>
-      <input type="number" value={input} />
+      <input type="text" value={input} readOnly />
       <section className={styles.container}>
-        <button onClick={handleInput}>7</button>
-        <button onClick={handleInput}>8</button>
-        <button onClick={handleInput}>9</button>
-        <button onClick={handleInput}>+</button>
-        <button onClick={handleInput}>6</button>
-        <button onClick={handleInput}>5</button>
-        <button onClick={handleInput}>4</button>
-        <button onClick={handleInput}>-</button>
-        <button onClick={handleInput}>3</button>
-        <button onClick={handleInput}>2</button>
-        <button onClick={handleInput}>1</button>
-        <button onClick={handleInput}>×</button>
-        <button onClick={onAllClean}>AC</button>
-        <button onClick={handleInput}>0</button>
-        {/* <button onClick={onCalculator}>=</button> */}
-        <button onClick={handleInput}>÷</button>
+        <button onClick={handleInput} value="7">
+          7
+        </button>
+        <button onClick={handleInput} value="8">
+          8
+        </button>
+        <button onClick={handleInput} value="9">
+          9
+        </button>
+        <button onClick={handleInput} value="+" className={styles.operator}>
+          +
+        </button>
+        <button onClick={handleInput} value="6">
+          6
+        </button>
+        <button onClick={handleInput} value="5">
+          5
+        </button>
+        <button onClick={handleInput} value="4">
+          4
+        </button>
+        <button onClick={handleInput} value="-" className={styles.operator}>
+          -
+        </button>
+        <button onClick={handleInput} value="3">
+          3
+        </button>
+        <button onClick={handleInput} value="2">
+          2
+        </button>
+        <button onClick={handleInput} value="1">
+          1
+        </button>
+        <button onClick={handleInput} value="*" className={styles.operator}>
+          ×
+        </button>
+        <button onClick={onAllClean} className={styles.setting}>
+          AC
+        </button>
+        <button onClick={handleInput} value="0">
+          0
+        </button>
+        <button onClick={onCalculator}>=</button>
+        <button onClick={handleInput} value="/" className={styles.operator}>
+          ÷
+        </button>
       </section>
     </div>
   );
 };
+
+export default Calculator;
--- src/hooks/useCalculator.js
+++ src/hooks/useCalculator.js
@@ -1,8 +1,44 @@
 import { useState } from "react";
+import { calculate } from "calculator-utils";
+import useToast from "./useToast";
 
 export default function useCalculator() {
+  // 입력 값
   const [input, setInput] = useState("");
-  const [result, setResult] = useState("");
+  // Toast 추가 함수
+  const { addToast } = useToast();
 
-  return { input, result };
+  // 입력 핸들러
+  const handleInput = (e) => {
+    const currentInput = e.target.value;
+
+    setInput((prev) => {
+      const newPrev = [...prev];
+      const lastInput = newPrev[newPrev.length - 1];
+
+      // 연산자 중복으로 입력 시, 마지막으로 선택한 연산자로 입력
+      if (isNaN(lastInput) && isNaN(currentInput)) {
+        newPrev.pop();
+        newPrev.push(currentInput);
+        return newPrev.join("");
+      } else return prev + currentInput;
+    });
+  };
+
+  // 모듈 사용해 계산
+  const onCalculator = () => {
+    const resultValue = calculate(input);
+
+    if (isNaN(resultValue)) {
+      addToast(resultValue);
+      onAllClean();
+    } else {
+      setInput(resultValue);
+    }
+  };
+
+  // 리셋
+  const onAllClean = () => setInput("");
+
+  return { input, handleInput, onCalculator, onAllClean };
 }
